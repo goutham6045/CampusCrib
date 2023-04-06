b725f0f8596d404d25f7149d7c9d8b0087ff9786
goutham6045 <goutham6045@.com>
2023-04-05 22:09:28 -0400

    fourth commit- made changes to contact and login

--- login-page.js
+++ login-page.js
@@ -1,17 +1,29 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+// const loginForm = document.getElementById("login-form");
+// const loginButton = document.getElementById("login-form-submit");
+// const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+// loginButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const username = loginForm.username.value;
+//     const password = loginForm.password.value;
 
-    if (username === "user" && password === "user") {
-        alert("You have successfully logged in.");
+//     if (username === "user" && password === "user") {
+//         alert("You have successfully logged in.");
         
-        location.reload();
+//         location.reload();
+//     } else {
+//         loginErrorMsg.style.opacity = 1;
+//     }
+// })
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if (username === "user" && password === "user") {
+      window.location.href = "index.html";
     } else {
-        loginErrorMsg.style.opacity = 1;
+      alert("Invalid username or password!");
     }
-})
+  }
+  
